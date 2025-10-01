f40ad87de1305dc12c48f45a5bf9c18ca3c66881
Username-Red <ashkestrel@gmail.com>
2025-10-01 17:42:32 +0200

    implementing daisyui, temporary product cards

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = ({ image="", name="" }) => {
+  return (
+    // <div className="bg-white rounded-lg shadow-md overflow-hidden p-4 hover:shadow-xl transition-shadow duration-300 my-5 flex flex-col items-center">
+    //   <img src={image} alt={name || "Product"} className="w-full h-48 object-cover rounded-md mb-4" />
+    //   {name && <h3 className="text-lg font-semibold mb-2 text-center">{name}</h3>}
+      
+    // </div>
+
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img src={image} alt={name || "Product"} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name || "Product"}</h2>
+        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+        <div className="card-actions justify-end">
+          <AddToCart />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,49 @@
 import Image from "next/image";
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer";
+import ProductCard from './components/ProductCard'
+import styles from './styles/Home.module.css'
 
 export default function Home() {
+  const products = [
+    ['Sebastian', '/images/Sebastian.png'], 
+    ['Lucy', '/images/Lucy.png'], 
+    ['Kris', '/images/Kris.png'], 
+    ['Jethro', '/images/Jet.png']
+  ] //Placeholder until database integration
+  
   return (
+
     <>
-      <Header/>
-      <div> Content will be here I promise</div>
-      <Footer/>
+      {/* <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-secondary">Get Started</button>
+          </div>
+        </div>
+      </div> */}
+      <div className={styles.gridContainer}>
+        
+        {products.map((p, i) => (
+          <div key={i} className={styles.card}>
+            <ProductCard image={p[1]} name={p[0]}/>
+          </div>
+        ))}
+        
+        
+      </div>
     </>
     
 
