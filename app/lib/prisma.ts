f90842946df808770b69c7ca4d6db5959bc5e7f4
Username-Red <ashkestrel@gmail.com>
2025-10-14 18:36:22 +0200

    possibly fixing prisma error

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// Type-safe global variable
+const globalForPrisma = global as unknown as { prisma?: InstanceType<typeof PrismaClient> };
 
+// Create or reuse Prisma client
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query"],
   });
 
+// Assign to global for hot-reload dev
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
