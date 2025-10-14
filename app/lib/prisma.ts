e6c256ac2036fead00a6b2950703d46d07b56ee3
Username-Red <ashkestrel@gmail.com>
2025-10-14 19:08:49 +0200

    trying again

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,14 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+// ./app/lib/prisma.ts
+import {PrismaClient} from "@prisma/client";
 
-// Type-safe global variable
-const globalForPrisma = global as unknown as { prisma?: InstanceType<typeof PrismaClient> };
+const globalForPrisma = globalThis as unknown as { prisma?: InstanceType<typeof PrismaClient> };
 
-// Create or reuse Prisma client
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query"],
   });
 
-// Assign to global for hot-reload dev
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
